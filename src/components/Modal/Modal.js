66039d57d9b2ef7868a2d84fc9450a145df1131f
Alexander Ilchenko <ilchenko.alexander.n@gmail.com>
2021-12-13 20:45:12 +0200

    add buttons fix

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,7 +2,7 @@ import { createPortal } from 'react-dom';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './Modal.module.css';
-import '../icons/arrow_back_ios_black_24dp.svg';
+import '../icons/arrow_back_ios_new_black_24dp.svg';
 import '../icons/arrow_forward_ios_black_24dp.svg';
 
 const modalRoot = document.querySelector('#modal-root');
@@ -31,11 +31,11 @@ export default class Modal extends Component {
     }
   };
 
-  handleLeft = event => {
+  handleLeft = () => {
     this.props.onArrowLeft();
   };
 
-  handleRight = event => {
+  handleRight = () => {
     this.props.onArrowRight();
   };
 
@@ -51,13 +51,13 @@ export default class Modal extends Component {
       <div className={s.overlay} onClick={this.handleBackDropClick}>
         <div className={s.modal}>
           <button
-            className={s.left}
+            className={s.leftarrow}
             type="button"
             onClick={() => this.handleLeft()}
           ></button>
           <img src={imageModal} alt="" />
           <button
-            className={s.right}
+            className={s.rightarrow}
             type="button"
             onClick={() => this.handleRight()}
           ></button>
